2c5ff3f783df474054de9cc64232020145fd811e
A21AdanFF <A21AdanFF@iessanclemente.net>
2022-09-15 10:21:34 +0200

    Test Code Viewer

--- test-1/astro.config.mjs
+++ test-1/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), solidJs()]
+  integrations: [mdx(), tailwind(), solidJs(), svelte()]
 });
--- test-1/src/components/Codes.jsx
+++ test-1/src/components/Codes.jsx
@@ -1,10 +1,17 @@
-function Codes({codes}) {
+function Codes({ codes }) {
     return (
-        <div>
-            {
-                codes.map(c=><div>{c["Code"]}</div>)
-            }
-        </div>
+        <>
+            <h1 className="text-2xl font-bold">Useless Codes</h1>
+            <div className="border-black border-[1px] p-2 rounded-sm grid grid-cols-2 gap-2 shadow-md">
+                {
+                    codes.map(c => <div className=
+                        "flex justify-center border-black border-[1px] p-1 rounded-sm shadow-md \
+                    transition-all duration-200 hover:bg-zinc-100 \
+                    cursor-pointer"
+                    >{c["Code"]}</div>)
+                }
+            </div>
+        </>
     )
 }
 
